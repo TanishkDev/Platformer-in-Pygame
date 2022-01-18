fa6954ccaaad8f680e76104dd27432507fa462e6
TanishkDev <anynmoussirohi@gmail.com>
2022-01-18 12:29:52 +0530

    animation

---
+++ game_data/levels/level_data/coin.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="coin" tilewidth="8" tileheight="8" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="2">
+  <image width="8" height="8" source="../../images/SpriteSheets/coin.png"/>
+ </tile>
+</tileset>
--- player.py
+++ player.py
@@ -6,23 +6,28 @@ from setting import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        self.player_idle_img = import_cut_graphics(
-            "/home/tanishk/Desktop/Pygame/Platformer/game_data/images/Character/Idle.png")
-        self.player_death_img = import_cut_graphics(
-            "/home/tanishk/Desktop/Pygame/Platformer/game_data/images/Character/Proto-Death.png")
-        self.player_jump_img = import_cut_graphics(
-            "/home/tanishk/Desktop/Pygame/Platformer/game_data/images/Character/Proto-jump.png")
-        self.player_run_img = import_cut_graphics(
-            "/home/tanishk/Desktop/Pygame/Platformer/game_data/images/Character/Proto-run.png")
+        self.animations = {"idle": import_cut_graphics(
+            "/home/tanishk/Desktop/Pygame/Platformer/game_data/images/Character/Idle.png"),
+            "death": import_cut_graphics(
+                "/home/tanishk/Desktop/Pygame/Platformer/game_data/images/Character/Proto-Death.png"),
+            "jump": import_cut_graphics(
+                "/home/tanishk/Desktop/Pygame/Platformer/game_data/images/Character/Proto-jump.png"),
+            "run": import_cut_graphics(
+                "/home/tanishk/Desktop/Pygame/Platformer/game_data/images/Character/Proto-run.png")}
 
         self.speed = 5
         self.frame_index = 0
+        self.animation_speed = 0.15
         self.gravity = 0.8
         self.jump_speed = -16
+        self.status = "idle"
 
-        self.image = self.player_idle_img[self.frame_index]
+        self.on_right = True
+        self.on_left = False
+        self.on_ground = True
+
+        self.image = self.animations["idle"][self.frame_index]
         self.scale_img()
-
         self.rect = self.image.get_rect(topleft=(pos))
         self.direction = pygame.math.Vector2(0, 0)
 
@@ -33,16 +38,41 @@ class Player(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_RIGHT]:
+            self.on_right = True
             self.direction.x = 1
 
+
         elif keys[pygame.K_LEFT]:
+            self.on_right = False
             self.direction.x = -1
+
         else:
             self.direction.x = 0
 
         if keys[pygame.K_SPACE]:
+            self.on_ground = False
             self.jump()
-     
+
+    def animate_player(self):
+        animation = self.animations[self.status]
+
+        self.frame_index += self.animation_speed
+        if self.frame_index > len(animation):
+            self.frame_index = 0
+
+        self.image = animation[int(self.frame_index)]
+
+        self.scale_img()
+
+    def get_status(self):
+        if self.direction.y < 0:
+            self.status = "jump"
+        else:
+            if self.direction.x != 0:
+                self.status = 'run'
+            else:
+                self.status = "idle"
+
     def apply_gravity(self):
         self.direction.y += self.gravity
         self.rect.y += self.direction.y
@@ -52,6 +82,8 @@ class Player(pygame.sprite.Sprite):
         self.rect.y += self.direction.y
 
     def update(self):
-        self.get_input()    
+        self.get_input()
         self.apply_gravity()
+        self.get_status()
+        self.animate_player()
         self.rect.x += self.direction.x * self.speed
